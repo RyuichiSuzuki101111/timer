40bdbe2354b403fb34abc163ebd2942d489436f5
Ryuichi Suzuki <untitled.tex@gmail.com>
2023-10-21 20:57:44 +0900

    wait_untilの非同期バージョンを作成

--- time_manager/time_manager.py
+++ time_manager/time_manager.py
@@ -2,86 +2,175 @@ from __future__ import annotations
 
 import time
 from types import TracebackType
-from typing import Callable, Optional, ParamSpec
+from typing import Callable, Coroutine, Optional, ParamSpec
+import asyncio
+from typing import overload
 
 P = ParamSpec('P')
 
 
-class TimerConfig:
-    timeout: float = 2e7
-    dt: float = 2e-7
+class _TimerConfig:
+    timer_configs: dict[str, _TimerConfig] = {}
+    default_timeout: float = 2e6
+    default_polling_time: float = 2e-6
 
-    def __init__(self, timeout: Optional[float], dt: Optional[float]) -> None:
-        if dt is None:
-            self.dt = TimerConfig.dt
+    def __init__(
+        self, timeout: Optional[float], polling_time: Optional[float]
+    ) -> None:
+        if polling_time is None:
+            self.polling_time = _TimerConfig.default_polling_time
         else:
-            self.dt = dt
+            self.polling_time = polling_time
 
         if timeout is None:
-            self.timeout = TimerConfig.timeout
+            self.timeout = _TimerConfig.default_timeout
         else:
             self.timeout = timeout
 
 
-def update_timer_defaults(
-    timeout: Optional[float] = None, dt: Optional[float] = None
+def set_timer_defaults(
+    timer_context_key: str = 'global',
+    timeout: Optional[float] = None,
+    polling_time: Optional[float] = None,
 ) -> None:
-    if timeout is not None:
-        TimerConfig.timeout = timeout
-    if dt is not None:
-        TimerConfig.dt = dt
+    match timer_context_key:
+        case 'global':
+            if polling_time is not None:
+                _TimerConfig.default_polling_time = polling_time
+            if timeout is not None:
+                _TimerConfig.default_timeout = timeout
+        case _:
+            _TimerConfig.timer_configs[timer_context_key] = _TimerConfig(
+                timeout, polling_time
+            )
 
 
 class Timer:
+    @overload
+    def __init__(self, task_label: Optional[str] = None):
+        pass
+
+    @overload
     def __init__(
         self,
-        timeout: Optional[float] = None,
-        dt: Optional[float] = None,
         task_label: Optional[str] = None,
+        *,
+        timer_context_key: str = 'global',
     ):
-        self._config: TimerConfig = TimerConfig(timeout, dt)
+        pass
+
+    @overload
+    def __init__(
+        self,
+        task_label: Optional[str] = None,
+        *,
+        timeout: Optional[float] = None,
+        polling_time: Optional[float] = None,
+    ):
+        ...
+
+    def __init__(
+        self,
+        task_label: Optional[str] = None,
+        *,
+        timer_context_key: Optional[str] = None,
+        timeout: Optional[float] = None,
+        polling_time: Optional[float] = None,
+    ):
+        if timer_context_key is None:
+            timer_context_key = 'global'
+
+        timer_config = _TimerConfig.timer_configs.get(timer_context_key)
+        if timer_config is None:
+            raise ValueError(
+                f'timer_context_keyに未登録のコンテキストキー{timer_context_key}'
+                'が設定されています。'
+            )
+
+        if timeout:
+            self._timeout = timeout
+        else:
+            self._timeout = timer_config.timeout
+
+        if polling_time:
+            self._polling_time = polling_time
+        else:
+            self._polling_time = timer_config.polling_time
+
         self.task_label = task_label
-        self.start_timestamp = 0.0
-        self.end_timestamp = 0.0
+        self.start_timestamp: float
+        self.end_timestamp: float
+        self.start()
 
     @property
-    def dt(self) -> float:
-        return self._config.dt
+    def polling_time(self) -> float:
+        return self._polling_time
 
     @property
     def timeout(self) -> float:
-        return self._config.timeout
+        return self._timeout
 
-    def start(self) -> None:
+    def start(self) -> Timer:
+        """タイムアウトの時刻を(再)設定して、自身を返す。
+        Timerクラスがコンテキストマネージャであるため、
+
+        with timer.start():
+            await timer.wait_until_async(some_condition, arg1, arg2)
+
+        のような使い方ができる。
+        """
         self.start_timestamp = time.time()
         self.end_timestamp = self.start_timestamp + self.timeout
-
-    def sleep(self) -> None:
-        time.sleep(self.dt)
+        return self
 
     def check_timeout(self) -> bool:
+        """現在の時刻がタイムアウト時刻を超えているかどうかを判定する。"""
         return self.end_timestamp <= time.time()
 
     def timeout_error(self) -> TimeoutError:
+        """TimeoutError オブジェクトを作成する。"""
         if self.task_label is None:
-            return TimeoutError('task timeout!')
+            return TimeoutError('タスクがタイムアウトしました。')
 
-        return TimeoutError(f"task: '{self.task_label}' timeout!")
+        return TimeoutError(f"タスク'{self.task_label}'がタイムアウトしました。")
 
     def wait_until(
-        self, condition: Callable[P, bool], *args: P.args, **kwargs: P.kwargs
+        self,
+        condition: Callable[P, bool],
+        *args: P.args,
+        **kwargs: P.kwargs,
     ) -> None:
-        """受け取った関数がTrueを返すまでループする。
-        タイムアウトになった場合, TimeoutErrorを発生させる
+        """
+        受け取った関数がTrueを返すまでループする。
+        タイムアウトになった場合, TimeoutErrorを発生させる。
         """
         while not self.check_timeout():
             if condition(*args, **kwargs):
                 return
-            self.sleep()
+
+            time.sleep(self.polling_time)
+
+        raise self.timeout_error()
+
+    async def wait_until_async(
+        self,
+        condition: Callable[P, Coroutine[bool, None, None]],
+        *args: P.args,
+        **kwargs: P.kwargs,
+    ) -> None:
+        """
+        受け取った非同期関数がTrueを返すまでループする。
+        タイムアウトになった場合, TimeoutErrorを発生させる。
+        """
+        while not self.check_timeout():
+            if await condition(*args, **kwargs):
+                return
+
+            await asyncio.sleep(self.polling_time)
+
         raise self.timeout_error()
 
     def __enter__(self) -> Timer:
-        self.start()
         return self
 
     def __exit__(
@@ -89,6 +178,5 @@ class Timer:
         exc_type: type[Exception],
         exc_value: Exception,
         exc_traceback: TracebackType,
-    ) -> None:
-        if exc_value is not None:
-            raise exc_value
+    ) -> bool:
+        return False
